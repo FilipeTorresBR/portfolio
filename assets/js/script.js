d5114270d37a85864f6e79bc9821cf30cbe8fcfc
Filipe Torres <felipetorres2405@gmail.com>
2023-02-03 18:51:20 -0300

    Adicionando localstorage para contar visitas no site e mostrar o easter egg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
 const audio = new Audio();
-audio.src = "../sound/joey-easteregg.mp3"
+audio.src = "assets/sound/joey-easteregg.mp3"
+
+function toogleEaster() {
+    var element = document.getElementById("easteregg-btn");
+    element.classList.toggle("show");
+}
+function checkLocalStorage(){
+    let number = localStorage.getItem('visitNumber');
+    if(number == 0){
+        toogleEaster();
+    }
+    else{
+        localStorage.setItem('visitNumber', 0);
+    }
+}
+
+  window.onload = checkLocalStorage;
